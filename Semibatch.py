2345f513656356a2e43becf6049436fa50d600bf
Oliver Ingold <oliver.ingold@gmail.com>
2020-06-05 15:47:44 -0400

    Introduced PI controller, working example

--- Semibatch.py
+++ Semibatch.py
@@ -11,62 +11,88 @@ import matplotlib.pyplot as plt
 import math
  
 # function returning dydt
-def model(y,t,F,caF,rho,cp,dHr,Q,Tf,k0,Ea,Tstart,Tset,Kc,Ki):
+def model(y,t,F,caF,rho,cp,dHr,Tf,k0,Ea,Tstart,Tset,Kc,Ki,Qmax):
         Na = y[0]    # Concentration of component A (mol/L)
         Nb = y[1]    # Concentration of component B (mol/L)
+      
         V = y[3]     # Reactor volume (L)
-        T = y[4]     # Reactor temperature (K)
-        Q = y[5]     # Heating / cooling power (W)
-        if T < Tstart:
+        Tfun = y[4]     # Reactor temperature (K)
+        eint = y[5]     # Integrated control error (K*s)
+        T_error = Tfun-Tset
+
+        #if Tfun <= Tset:
+        #        cont = abs(Kc*T_error+Ki*eint) # PI controller value
+        #else:
+        cont = Kc*T_error+Ki*eint 
+
+        if cont==0:
+                Q = 0
+        else: 
+                Q = Qmax*cont/abs(1+cont) # Heating / cooling power (W)
+        #else:
+         #       Q = Qmax*cont/(-1+cont)
+        #print("Temp = " + str(Tfun))
+        #print("cont = " + str(cont))
+        #print("Q = " + str(Q))
+        
+
+        if Tfun < Tstart:
                 F = 0   # Feed only starts at starting temperature
-        k = k0*math.exp(-Ea/(8.3145*T))        # Reaction rate constant (L/mol/s)
+        k = k0*math.exp(-Ea/(8.3145*Tfun))        # Reaction rate constant (L/mol/s)
         dNadt =  F*caF-k*Na*Nb/V     # molar amount change of component A [mol/L/s]
         dNbdt =  -k*Na*Nb/V          # molar amount change of component A [mol/L/s]
         dNcdt =  k*Na*Nb/V           # molar amount change of component A [mol/L/s]
         dVdt =  F                    # Volume change rate (L/s)
-        dTdt = (F*rho*cp*(Tf-T)+k*Na*Nb*dHr/V+Q)/(V*rho*cp) # rate of temperature change [K]
-        dQdt = Kc+Ki*(T-Tset)
- #       dQdt = 0.0001
-        return [dNadt,dNbdt,dNcdt,dVdt,dTdt,dQdt]
+        dTdt = (F*rho*cp*(Tf-Tfun)+k*Na*Nb*dHr/V+Q)/(V*rho*cp) # rate of temperature change [K]
+        deintdt = T_error              # Integration of control error (K*s)
+        #print(k)
+        return [dNadt,dNbdt,dNcdt,dVdt,dTdt,deintdt]
  
-# intial conditions
-ca0 = 0.0    # Concentration of component A (mol/L)
-cb0 = 0.1    # Concentration of component B (mol/L)
-cc0 = 0      # Concentration of component C (mol/L)
-V0 = 6000    # Reactor volume (L)
-T0 = 298     # Starting temperature (K)
-k0 = 1        # Arrhenius constant (L/mol/s)
-Ea = 1.6*10**5 # activation energy (J/mol)
-Q0 = 0       # Initial heating/cooling (W)
-y0 = [ca0*V0,cb0*V0,cc0*V0,V0,T0,Q0] # vector of initial conditions
-
-#Physical data
-rho = 1       # density of the solvent (kg/L)
-dHr = 1400000 # enthalpy of reaction (J/mol)
-cp = 4186     # heat capacity of solvent (J/kg/K)
-
 # Parameters
-F = 5     # Feed rate (L/s)
-caF = 0.1 # Feed concentration of A (L/s)
+F = 0.5     # Feed rate (L/s)
+caF = 5 # Feed concentration of A (L/s)
 Tf = 298  # Temperature of the feed (K)
-Q = 20000 # cooling power (W)
 Tstart = 298  # Starting temperature of the feed [K]
 Tset = 273+80 # Setpoint temperature of PI controller
 Kc = -1 # PI controller proportional constant
-Ki = -1 # PI controller integral constant
+Ki = -0.0001 # PI controller integral constant
+Qmax = 1000000 # Maximal heating / cooling power (W)
+
+# intial conditions
+ca0 = 0.0    # Concentration of component A (mol/L)
+cb0 = 2    # Concentration of component B (mol/L)
+cc0 = 0      # Concentration of component C (mol/L)
+V0 = 2000    # Reactor volume (L)
+T0 = 298     # Starting temperature (K)
+k0 = 10**9   # Arrhenius constant (L/mol/s)
+Ea = 13000    # activation energy (J/mol)
+p0 = Kc*(T0-Tset)   # Initial heating/cooling (W)
+y0 = [ca0*V0,cb0*V0,cc0*V0,V0,T0,p0] # vector of initial conditions
+
+#Physical data
+rho = 1       # density of the solvent (kg/L)
+dHr = 140000 # enthalpy of reaction (J/mol)
+cp = 4186     # heat capacity of solvent (J/kg/K)
+
 
 
 # time points
-t = np.linspace(0,3900)
+t = np.linspace(0,1800)
 
 # solving ODE
-y = odeint(model,y0,t,args=(F,caF,rho,cp,dHr,Q,Tf,k0,Ea,Tstart,Tset,Kc,Ki))
+y = odeint(model,y0,t,args=(F,caF,rho,cp,dHr,Tf,k0,Ea,Tstart,Tset,Kc,Ki,Qmax))
 
 Na = y[:,0]
 Nb = y[:,1]
 Nc = y[:,2]
 V = y[:,3]
 T = y[:,4]
+p = y[:,5]
+
+T_error = T-Tset
+cont = Kc*T_error+Ki*np.trapz(T_error,t)
+Q = Qmax*cont/abs(1+cont)
+
 ca = Na/V
 cb = Nb/V
 cc = Nc/V
@@ -84,4 +110,12 @@ plt.show
 plt.close
 
 plt.figure()
-plt.plot(t,T)
+plt.plot(t,T-273.15)
+plt.xlabel('Time (s)')
+plt.ylabel('Temperature (deg C)')
+
+plt.figure()
+plt.plot(t,T-273.15)
+plt.plot(t,p)
+plt.xlabel('Time (s)')
+plt.ylabel('Integrated temperature error (deg C)')
